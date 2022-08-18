457648ceae534cc565c94eb80f90c0342250db50
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-08-18 15:04:33 +0900

    Mapped Superclass - 매핑 정보 상속

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,11 +1,12 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Member {
+public class Member extends BaseEntity{
 
     @Id
     @GeneratedValue
@@ -25,7 +26,6 @@ public class Member {
 
     @OneToMany(mappedBy = "member")
     private List<MemberProduct> memberProducts = new ArrayList<>();
-
 
     public Long getId() {
         return id;

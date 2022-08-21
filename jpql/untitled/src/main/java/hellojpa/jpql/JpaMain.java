68bfd7a7bf0a36f57d8529e0c4a0d418f2702e44
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-08-21 11:28:51 +0900

    프로젝션(SELECT)

--- jpql/untitled/src/main/java/hellojpa/jpql/JpaMain.java
+++ jpql/untitled/src/main/java/hellojpa/jpql/JpaMain.java
@@ -20,11 +20,16 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
+            em.flush();
+            em.clear();
 
-            System.out.println("result = " + result.getUsername());
+            List<MemberDTO> result = em.createQuery("select new hellojpa.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
+
 
             tx.commit();
         } catch (Exception e) {
